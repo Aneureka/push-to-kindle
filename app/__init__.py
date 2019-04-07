3c755a40992afeaa1e7be2d007c9519fc1bb3412
Hiki <aneureka2@gmail.com>
2019-04-07 11:57:24 +0800

    Update homepage and add cors
    Remove some fonts

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, current_app
+from flask_cors import CORS
 
 from config import config
 
 
 def create_app(config_name='default'):
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(config[config_name])
 
     config[config_name].init_app(app)
